9a07d309786212920b0240e2589867fad9461bfe
Averon Dekker <41345@ma-web.nl>
2026-01-13 12:47:25 +0100

    js2.2 handle the array

---
+++ js2.2 handle the array/main.js
@@ -0,0 +1,16 @@
+let arr = ["appel", 5, true, "peer", 10];
+
+console.log(arr);
+
+console.log(arr.length);
+
+console.log(arr.push("banaan"));
+console.log(arr);
+
+console.log(arr.pop());
+console.log(arr);
+
+console.log(arr.includes(true));
+console.log(arr);
+
+console.log(arr.join(", "));
